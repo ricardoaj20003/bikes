dc15efb40cc1b061a8e75db378d11dc60691f2ca
Oscar Hugo Cardenas Lopez <ohcl87@hotmail.com>
2018-11-15 14:01:29 -0600

    add paymente detail

--- api_code/app/routes/pedidos.js
+++ api_code/app/routes/pedidos.js
@@ -1,20 +1,20 @@
 const prefix = '/pedidos',
       Pedido = require('../models/pedido').Pedido,
       Address = require('../models/address').Address,
+      PaymentDetail  = require('../models/payment_detail.js').PaymentDetail,
       Person  = require('../models/person').Person;
 
 
 module.exports = function(fastify, opts, next){
-  fastify.get(`${prefix}`,
-    {
-      schema: {
-        description: 'Optener todos los pedidos',
-        tags: ['Pedidos'],
-        summary: 'Retorna los pedidos',
-      }
-    },
-    (request, response) => {
+  fastify.get(`${prefix}`, (request, response) => {
       return response.send({ hello: 'world' });
+    });
+
+  fastify.get(`${prefix}/:id`, (request, response) => {
+    return Pedido.where(request.params).fetch({withRelated: ['address']})
+      .then(function(pedido){
+        return response.send(pedido);
+      });
     });
 
   fastify.post(`${prefix}`, (request, response) => {
@@ -30,6 +30,28 @@ module.exports = function(fastify, opts, next){
       })
       .catch(function (err) {
         return response.send(err);
+      });
+  });
+
+  fastify.post(`${prefix}/:id/add_person`, (request, response) => {
+    let pedidoId = request.params.id;
+    return new Person(request.body.person).save({'pedido_id': pedidoId})
+      .then(function (person){
+        return Pedido.where(request.params).fetch({withRelated: ['address', 'person']})
+          .then(function(pedido){
+            return response.send(pedido);
+          });
+      });
+  });
+
+  fastify.post(`${prefix}/:id/add_payment_detail`, (request, response) => {
+    let pedidoId = request.params.id;
+    return new PaymentDetail(request.body.payment_detail).save({'pedido_id': pedidoId})
+      .then(function (PaymentDetail){
+        return Pedido.where(request.params).fetch({withRelated: ['address', 'person', 'paymentDetail']})
+          .then(function(pedido){
+            return response.send(pedido);
+          });
       });
   });
 
