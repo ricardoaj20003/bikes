676545caadd27c7fdf62d20d3ec534b480d75af6
Jeaneth Farmer <xaid27@gmail.com>
2018-11-27 16:52:44 -0600

    min css images missing

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 
 module.exports = {
-  entry: './js/app/index.js',
+  entry: ['./js/app/index.js', './css/base.css'],
   mode: 'development',
   output: {
     filename: 'bundle.js',
@@ -21,8 +23,37 @@ module.exports = {
         },{
           loader: 'expose-loader',
           options: '$'
-        }]
-      }
+        }
+        ],
+      },{
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          loader: 'css-loader',
+          options: {
+            minimize: true,
+          },
+        }),
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true 
+            }
+            //options: {
+              //name: '[path][name].[ext]',
+              //emitFile: false,
+            //},
+          },
+        ]
+      },
+      {
+        test: /\.(svg|woff|woff2|eot|ttf|otf)$/,
+        loader: 'url-loader',
+        options: {},
+      },
     ]
   },
   plugins: [
@@ -30,6 +61,23 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
-    })
-  ]
+    }),
+    new ExtractTextPlugin( {
+      filename: '../css/bundle.css',
+    }),
+    //new UglifyJsPlugin({
+      //cache: true,
+      //parallel: true,
+      //uglifyOptions: {
+        //compress: false,
+        //ecma: 6,
+        //mangle: true,
+      //},
+      //sourceMap: true,
+    //}),
+  ],
+  //resolve: {
+    //extensions: ['', '.js', '.scss'],
+    //modulesDirectories: ['js', 'node_modules']
+  //}
 };
