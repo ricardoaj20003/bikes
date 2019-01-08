5a9b3c1071b4631c2619cadcbfe745adbe0f5833
Your Name <you@example.com>
2019-01-08 20:04:13 +0000

    ajuste servidor envio de mensajes

--- api_code/migrations/20190107114204_addActiveToOrder.js
+++ api_code/migrations/20190107114204_addActiveToOrder.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  knex.schema.table('orders', table => {
+  return knex.schema.table('orders', table => {
     table.boolean('active').defaultTo(true);
   });
 };
 
 exports.down = function(knex, Promise) {
-  knex.schema.table('orders', table => {
+  return knex.schema.table('orders', table => {
     table.dropColumn('active');
   });
 };
