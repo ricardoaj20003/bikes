eda06cf958700c69c8c8a1572af5dc8ad955c4c2
Oscar Hugo Cardenas Lopez <ohcl87@hotmail.com>
2019-05-03 17:08:28 -0500

    estableciendo logica login con sessiones

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ var fs = require("fs");
 var host = "localhost";
 var port = 3003;
 var express = require("express");
-var path = require("path");
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var app = express();
+
+app.use(session({
+  name: 'server-session-cookie-id',
+  secret: 'my express secret',
+  saveUninitialized: true,
+  resave: true,
+  store: new FileStore(),
+  cookie: { maxAge: 6000 }
+}));
 
 app.use(express.static(__dirname + '/app'));
 
@@ -12,9 +22,17 @@ app.set('views', __dirname + '/app/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+app.use(function (req, res, next) {
+  if (typeof req.session.views === 'undefined')
+    req.session.views = 1;
+  else
+    req.session.views += 1;
+
+  next();
+});
 
 app.get('/', (req, res) => {
-  res.render('index.html');
+  res.render('index.html', {data: 'algo'});
 })
 app.get('/admin', (req, res) => {
   res.render('admin.html');
@@ -46,6 +64,30 @@ app.get('/landing', (req, res) => {
 app.get('/reporte_semanal', (req, res) => {
   res.render('reporte_semanal.html');
 })
+
+app.post('/sign_in', (req, res) => {
+  let axios = require('axios');
+
+  const requestConfig = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+
+  return axios.post('http://localhost:3000/users/sign_in', {
+    "username": "sorsucrelFinal",
+    "password": "123456789"
+  }, requestConfig)
+    .then(function (response) {
+      console.log(response.data);
+      return res.send({});
+    })
+    .catch(function (error) {
+      return res.send({}).status(401);
+    });
+});
+
 app.get('*', (req, res) => {
   res.redirect('/');
 })
